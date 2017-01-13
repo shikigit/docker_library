93515aaf9e610d0ec6314923607b3111f687b2a4
zengshihao <zengshihao.yidian-inc.com>
2017-01-13 16:12:00 +0800

    update

--- export.py
+++ export.py
@@ -12,7 +12,7 @@ etcd-amd64:3.0.14-kubeadm
 kube-dnsmasq-amd64:1.10.1
 exechealthz-amd64:1.2
 pause-amd64:3.0
-kubernetes-dashboard-amd64:v1.4.0"""
+kubernetes-dashboard-amd64:v1.5.1"""
 
 for image in image_str.strip().split():
     image_name = image.strip().split(':')[0]
